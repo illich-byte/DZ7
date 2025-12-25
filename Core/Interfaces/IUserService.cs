a9c586f80bf7c6565ae44bcd9f8d9c213433fdb3
illich-byte <illich1907@gmail.com>
2025-12-25 15:19:54 +0200

    Add SearchUsersAsync method to IUserService
    Added SearchUsersAsync method to IUserService interface.

--- Core/Interfaces/IUserService.cs
+++ Core/Interfaces/IUserService.cs
@@ -1,8 +1,10 @@
-﻿using Core.Models.Account;
+using Core.Models.Account;
 
 namespace Core.Interfaces;
 
 public interface IUserService
 {
     Task<UserProfileModel> GetUserProfileAsync();
+    // Додаємо метод пошуку
+    Task<List<UserProfileModel>> SearchUsersAsync(string search);
 }
