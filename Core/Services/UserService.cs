9295294b49833e09b5ad7a151e560a58db9c59bb
illich-byte <illich1907@gmail.com>
2025-12-25 15:19:42 +0200

    Refactor UserService and add user search feature
    Refactored GetUserProfileAsync method and added SearchUsersAsync method for user searching functionality.

--- Core/Services/UserService.cs
+++ Core/Services/UserService.cs
@@ -1,8 +1,8 @@
-﻿using Core.Interfaces;
+using Core.Interfaces;
 using Core.Models.Account;
 using Domain.Entities.Idenity;
 using Microsoft.AspNetCore.Identity;
-using System.Security.Claims; 
+using Microsoft.EntityFrameworkCore; // Важливо для ToListAsync
 
 namespace Core.Services;
 
@@ -12,24 +12,45 @@ public class UserService(IAuthService authService,
     public async Task<UserProfileModel> GetUserProfileAsync()
     {
         var userId = await authService.GetUserIdAsync();
-
         var user = await userManager.FindByIdAsync(userId.ToString());
 
-        if (user == null)
-        {
-            throw new Exception("User not found.");
-        }
+        if (user == null) throw new Exception("User not found.");
 
-        var model = new UserProfileModel
-        {
-            Id = user.Id,
-            Email = user.Email!,
-            UserName = user.UserName!,
-            FirstName = user.FirstName,
-            LastName = user.LastName,
-            Image = user.Image
-        };
-
-        return model;
+        return MapToProfile(user);
     }
+
+    // РЕАЛІЗАЦІЯ ПОШУКУ
+    public async Task<List<UserProfileModel>> SearchUsersAsync(string search)
+    {
+        var searchLower = search.ToLower();
+
+        // Шукаємо користувачів, де email, ім'я або прізвище містять пошуковий запит
+        var users = await userManager.Users
+            .Where(u => u.Email!.ToLower().Contains(searchLower) || 
+                        u.FirstName!.ToLower().Contains(searchLower) || 
+                        u.LastName!.ToLower().Contains(searchLower))
+            .Select(u => new UserProfileModel // Мапимо відразу в модель профілю
+            {
+                Id = u.Id,
+                Email = u.Email!,
+                UserName = u.UserName!,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                Image = u.Image
+            })
+            .ToListAsync();
+
+        return users;
+    }
+
+    // Допоміжний метод для мапінгу (щоб не дублювати код)
+    private UserProfileModel MapToProfile(UserEntity user) => new UserProfileModel
+    {
+        Id = user.Id,
+        Email = user.Email!,
+        UserName = user.UserName!,
+        FirstName = user.FirstName,
+        LastName = user.LastName,
+        Image = user.Image
+    };
 }
